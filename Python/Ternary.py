273fe304dd64920a1c1545c3e531fb07bdcca80d
Shri <shrikrushnakhedkar07@gmail.com>
2021-12-19 13:53:56 +0530

    git add * git commit -m "1st success"

--- Python/Ternary.py
+++ Python/Ternary.py
@@ -1,111 +1,183 @@
+# class P:
+#     def m1(self):
+#         print("Parent class method")
+# class C(P):
+#     def m1(self):
+#         super().m1()
+#         print("Child class method")
+# c = C()
+# c.m1()
 
-# 1. Ternary operator
-# a = int(input("Enter 1st No"))
-# b = int(input("Enter 2nd No"))
-# c = int(input("Enter 3rd No"))
-# minimum = a if a<b and b<c else  b if b<c else c
-# print(minimum)
-#--------------------------------------------------------------
-
-# 2. Product of two number
-# a,b,c = [int(x) for x in input("Enter 3 number: ").split()]
-# print("Product of 3 number:",a*b*c)
-
-#--------------------------------------------------------------
-
-# n = int(input("Enter number of rows:"))
-# for i in range(1,n+1):
-#     print(" "*(n-i),end="")
-#     print("*"*i)
-
-#--------------------------------------------------------------
-
-# cart = [10,20,40,500,700,1,2,7,40]
-# for x in cart:
-#     if x<500:
-#         print(x)
-#         continue
-#     print("not in range",x)
-
-#--------------------------------------------------------------
-#Substring program:-
-
-# s = "babjbfjasufhjndfvasopdababananaabab"
-# sub = "ab"
-# pos = s.find(sub)
-# c = 0
-# while pos != -1:
-#     c = c + 1
-#     print("Substring occur at:", pos)
-#     pos = s.find(sub,pos+len(sub),len(s))
-#
-# print('The Number of occurrences:',c)
-
-
-# s='abcdefabghijkabjkhlm'
-# subs='ab'
-# c= s.count(subs)
-# print('The Number of occurrences:',c)
+# s = "shrikrushna"
+# i = len(s)-1
+# r = ""
+# while i>=0:
+#     r = r+s[i]
+#     i = i-1
+# print(r)
 
 #
-# s=input('Enter Any String to reverse:')
-# r=reversed(s)
-# output = "".join(r)
-# print(output)
-# print(s[::-1])
-
-# input: Learning Python is very easy
-# output: easy very is Python Learning
-
-#-----------------------------------------------------
-# def fact(n):
-#     result = 1
-#     if n == 1:
-#         return result
-#     while n > 1:
-#         result = result * n
-#         n = n-1
-#     return result
-# n = int(input("Enter number: "))
-# print(f"Factorial of number {n} is: ",fact(n))
-
-# def fact(n):
-#     result = 1
-#     if n == 0:
-#         return result
-#     else:
-#         result = n * fact(n-1)
-#     return result
-
-#---------------------------------------------------------
-d1 = {100:'durga',200:'ravi',300:'shiva',400:'pavan'}
-d2 = {500:'sunny',600:'bunny',700:'chinny'}
-
-# for k,v in d2.items():
-# 	d1[k]=v
+# l = [1,2,3,4,5,6,78,99,0,1]
+# max = l[0]
+# max1 = []
 #
-# #d1.update(d2)
+# for i in range(1,len(l)):
+#     if l[i]>max:
+#         max1 = max
+#         max = l[i]
+# print(max1)
+
+#Operator overloading
+# class Book:
 #
-# print(d1)
+#     def __init__(self,page):
+#         self.page = page
+#
+#     def __add__(self, other):
+#         result  = self.page + other.page
+#         rbook = Book(result)
+#         return rbook
+#     def __str__(self):
+#         return str(self.page)
+# #
+# b1 = Book(10)
+# b2 = Book(20)
+# print(b1+b2)
 
-#----------------------------------------------------------
-
-from random import *
-alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-digits='0123456789'
-print(choice(alphabets+digits)+choice(alphabets+digits)+choice(alphabets+digits)+choice(alphabets+digits))
-
-from random import *
-otp=''
-for i in range(6):
-	otp=otp+str(randint(0,9))
-print(otp)
+#Method overloading
+# class AA:
+#     def m1(self,arg1):
+#         print("This is m1 method")
+#     def m1(self,arg1,arg2):
+#         print("This is m1 method with arg1")
+#     def m1(self,arg1,arg2,arg3):
+#         print("This is m1 method with arg1,arg2 & arg3")
+# a = AA()
+# a.m1(1,2,3)
 
 
-from random import *
-alphanumeric='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-for i in range(10):
-	pwd=''
-	for i in range(6):
-		pwd=pwd+choice(alphanumeric)
-	print(pwd)
+# Abstraction
+# from abc import *
+# class Vehical(ABC):
+#     @abstractmethod
+#     def getNoofWheel(self):
+#         pass
+# class Bus(Vehical):
+#     def getNoofWheel(self):
+#         return 6
+#
+# b = Bus()
+# print(b.getNoofWheel())
+
+
+# a  =3,4,5
+# print(a)
+#
+# b1,b2,b3 = a
+# print(b1,b2,b3 )
+
+
+
+
+
+
+# class Parent1:
+#     def __init__(self,a):
+#         self.a = a
+#     def m1(self):
+#         print("Parent1 class m1 method")
+#
+#
+# class Parent2:
+#     def __init__(self,b):
+#         self.b = b
+#     def m2(self):
+#
+#         print("Parent2 class m2 method")
+#
+# class Child(Parent1,Parent2):
+#     def __init__(self,e):
+#         self.e = e
+#
+#     def m3(self):
+#         super().__init__(20)
+#
+#         print("Child class m3 method")
+#         print(self.e)
+#         print(self.b)
+#
+#
+#
+# c = Child(10)
+# c.m1()
+# c.m2()
+# c.m3()
+# print(c.a)
+
+n1 = input("Enter strng:")
+if n1 == n1[ : :-1]:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
+
+# n1 = input("Enter string:")
+#
+# result = lambda n1: n1 == n1[::-1]
+#
+# print("Palindrome")
+
+
+
+
+
+
+
+# n1 = input("Enter strng:")
+# r1 = reversed(n1)
+# print(r1)
+# rev = "".join(r1)
+# if n1 == rev:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+
+# class Parent:
+#     def __init__(self):
+#         self.__a1 = 10
+#
+#
+#     def __m1(self):
+#         print("This is Private method")
+#
+#     def m2(self):
+#         print(self.__a1)
+#         print(self.__m1())
+#         self._d = 30
+#
+# class Child(Parent):
+#     def m3(self):
+#         print(self._d)
+#
+#
+# p = Parent()
+# p.m2()
+# print(p._d)
+# # print(p.__a1)
+# # Parent(p.__m1())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
